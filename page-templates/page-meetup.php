f8cb1685b478212bae4e3942128daf3448c5b72d
jay-allen <allenjp7@gmail.com>
2017-05-24 20:06:09 -0500

    Added get_sidebar() functionality

--- page-templates/page-meetup.php
+++ page-templates/page-meetup.php
@@ -29,46 +29,9 @@ get_header(); ?>
 			?>
 
 		</main><!-- #main -->
-		<aside id="secondary" class="col-md-4 widget-area" role="complementary">
-			<?php //get_sidebar(); ?>
-				<section id="categories-2" class="widget widget_categories next-meetup">
-					<!-- <h2 class="widget-title text-center">Social Font Icons</h2> -->
-					<ul class="social-links list-inline text-center">
-						<li>
-									<a href="https://twitter.com/NWTCWebWorkers" target="_blank">
-											<i class="fa fa-twitter" aria-hidden="true"></i>
-									</a>
-							</li>
-						<li>
-									<a href="https://trello.com/invite/b/UKPSaGqx/d56c34b7e54ea729fb562129b3fcfcba/nwtc-web-workers" target="_blank">
-											 <i class="fa fa-trello" aria-hidden="true"></i>
-									 </a>
-							</li>
-						<li>
-									<a href="/email">
-											 <i class="fa fa-envelope" aria-hidden="true"></i>
-									 </a>
-							</li>
-					</ul>
 
-				</section>
-				<section id="categories-2" class="widget widget_categories next-meetup">
-					<h2 class="widget-title text-center">Next Meetup</h2>
-					<div class="date text-center">
-						<p>November 29th</p>
-						<p>6:00PM - 7:00PM</p>
-					</div>
+        <?php get_sidebar(); ?>
 
-				</section>
-				<!-- <section id="categories-2" class="widget widget_categories">
-					<h2 class="widget-title">Cool Events:</h2>
-					<div class="date">
-						<p>BarCamp GB</p>
-						<p>November 2016</p>
-					</div>
-
-				</section> -->
-			</aside>
 	</div><!-- #primary -->
 
 <?php
