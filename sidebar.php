ada19f0b9eb7228b2724d6b0d274c558ba1e02f3
jay-allen <allenjp7@gmail.com>
2017-05-24 20:00:59 -0500

    Updated sidebar to use get_sidebar()

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,25 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area" role="complementary">
+<aside id="secondary" class="col-md-4 widget-area" role="complementary">
+    <section id="categories-2" class="widget widget_categories next-meetup">
+        <ul class="social-links list-inline text-center">
+            <li>
+                <a href="https://twitter.com/NWTCWebWorkers" target="_blank">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                </a>
+            </li>
+            <li>
+                <a href="https://trello.com/invite/b/UKPSaGqx/d56c34b7e54ea729fb562129b3fcfcba/nwtc-web-workers" target="_blank">
+                    <i class="fa fa-trello" aria-hidden="true"></i>
+                </a>
+            </li>						
+        </ul>
+    </section>
+    <section class="widget widget_categories email-signup">
+        <p class="text-center">
+            <a href="/email">Subscribe to Meetup News</a>
+        </p>
+    </section>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
